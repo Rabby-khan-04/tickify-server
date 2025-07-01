c53327f2ed4fcc683ab1835179b789e4c2b2e7fe
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-07-01 19:01:10 +0600

    feat(user): add API to manage favorite movies (add, remove, fetch)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,8 @@ import User from "../models/user.model.js";
 import ApiResponce from "../utils/ApiResponse.js";
 import { cookieOptions } from "../constants.js";
 import jwt from "jsonwebtoken";
+import Movie from "../models/movie.model.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -212,12 +214,113 @@ const getAUser = asyncHandler(async (req, res) => {
   }
 });
 
+const addFavoriteMovies = asyncHandler(async (req, res) => {
+  try {
+    const incomingUser = req.user;
+    const { movieId } = req.params;
+
+    const movie = await Movie.findById(movieId);
+
+    if (!movie) throw new ApiError(status.NOT_FOUND, "Movie not found!!");
+
+    const user = await User.findById(incomingUser._id);
+
+    if (!user) throw new ApiError(status.NOT_FOUND, "User not found!!");
+
+    if (user.favorites.includes(movieId))
+      return res
+        .status(status.OK)
+        .json(new ApiResponce(status.OK, {}, "Movie already in favorites!!"));
+
+    user.favorites.push(movieId);
+    await user.save();
+
+    return res
+      .status(status.CREATED)
+      .json(new ApiResponce(status.OK, {}, "Movie added to favorite"));
+  } catch (error) {
+    console.log(`ERROR in add favorite movie: ${error}`);
+
+    if (error instanceof ApiError) throw error;
+
+    throw new ApiError(
+      status.INTERNAL_SERVER_ERROR,
+      "Something went wrong while adding movie to favorite!!"
+    );
+  }
+});
+
+const removeMovieFromFavorites = asyncHandler(async (req, res) => {
+  try {
+    const { movieId } = req.params;
+    const incomingUser = req.user;
+
+    const user = await User.findById(incomingUser._id);
+
+    if (user.favorites.includes(movieId)) {
+      const newFav = user.favorites.filter(
+        (favorite) => favorite.toString() !== movieId
+      );
+
+      user.favorites = newFav;
+      await user.save();
+
+      return res
+        .status(status.OK)
+        .json(new ApiResponce(status.OK, {}, "Movie removed from favorites!!"));
+    } else {
+      throw new ApiError(status.NOT_FOUND, "Movie does not exist in favorites");
+    }
+  } catch (error) {
+    console.log(`ERROR in removing movie from frovites: ${error}`);
+
+    if (error instanceof ApiError) throw error;
+
+    throw new ApiError(
+      status.OK,
+      "Something went wrong while removing movie from favorite list!!"
+    );
+  }
+});
+
+const getFavoriteMovies = asyncHandler(async (req, res) => {
+  try {
+    const incomingUser = req.user;
+
+    const user = await User.findById(incomingUser._id).populate({
+      path: "favorites",
+      select: "-casts -genres",
+    });
+
+    return res
+      .status(status.OK)
+      .json(
+        new ApiResponce(
+          status.OK,
+          user,
+          "Favorite movies fetched successfully!!"
+        )
+      );
+  } catch (error) {
+    console.log(`ERROR in fetching frovite moives: ${error}`);
+
+    if (error instanceof ApiError) throw error;
+
+    throw new ApiError(
+      status.OK,
+      "Something went wrong while fetching favorite movies!!"
+    );
+  }
+});
 const UserController = {
   registerUser,
   issueJWT,
   refreshAccessToken,
   logoutUser,
   getAUser,
+  addFavoriteMovies,
+  removeMovieFromFavorites,
+  getFavoriteMovies,
 };
 
 export default UserController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,5 +5,15 @@ import AuthMiddleware from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.route("/me").get(AuthMiddleware.verifyJwt, UserController.getAUser);
+router
+  .route("/favorite/:movieId")
+  .post(AuthMiddleware.verifyJwt, UserController.addFavoriteMovies);
+router
+  .route("/remove-favorite/:movieId")
+  .post(AuthMiddleware.verifyJwt, UserController.removeMovieFromFavorites);
+
+router
+  .route("/favorites")
+  .get(AuthMiddleware.verifyJwt, UserController.getFavoriteMovies);
 
 export default router;
